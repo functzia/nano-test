49a9060fc776d6e2d6170dc4977afdf529f2d30d
Alon Niv <oakfang@gmail.com>
2018-10-23 18:24:40 +0300

    test multi insert

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 module.exports = function(_request) {
+  if (Math.random() >= 0.5) {
+    return [
+      {
+        name: "foo",
+        ts: Date.now()
+      },
+      {
+        name: "bar",
+        ts: Date.now()
+      }
+    ];
+  }
   return {
     name: "foobar",
     ts: Date.now()
