d5cae069469dfd78cca682c76326b17a2962a3b9
Alon Niv <oakfang@gmail.com>
2018-10-24 15:01:53 +0300

    add test

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-module.exports = function(_request) {
-  if (Math.random() >= 0.5) {
-    return [
-      {
-        name: "foo",
-        ts: Date.now()
-      },
-      {
-        name: "bar",
-        ts: Date.now()
-      }
-    ];
-  }
-  return {
-    name: "foobar",
-    ts: Date.now()
-  };
+module.exports = function(document) {
+  this.log.info(`Document ${document._id} added`);
 };
